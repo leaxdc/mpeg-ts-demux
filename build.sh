d5aa3a9fd5c1d1293c804d74674aa959d2ecfaca
Peter Asanov <leaxdc@gmail.com>
2019-05-28 21:47:34 +0200

    bootstrap

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ ! -e build ]; then
+    mkdir build
+fi
+if [ "$1" = "" ]; then
+	build_type="Release"
+else
+	build_type="$1"
+fi
+
+pushd .
+cd build
+echo "BUILD CONFIGURATION: $build_type"
+cmake ../src -DCMAKE_BUILD_TYPE=$build_type
+cpu_core_count=$(grep -c '^processor' /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+make -j$cpu_core_count install
+popd
+
